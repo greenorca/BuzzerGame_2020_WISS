351dd49a3c793ad13045de3c84a15d1dd6fd9c0a
Sven Schirmer <schirmer@green-orca.com>
2020-07-08 06:49:31 +0200

    play button has fokus now

--- src/view/StartupViewController.java
+++ src/view/StartupViewController.java
@@ -4,6 +4,8 @@ import application.GameController;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 
 
@@ -13,14 +15,21 @@ public class StartupViewController{
 	//application getter setter
 	GameController gameController;
 	
+	@FXML Button btnPlay;
+	
 	@FXML
 	public void btnLobbyPressed(ActionEvent event) {
 		gameController.showLobbyView();
 		//gameController.spielrundeStarten();
 	}
 	
+	@FXML public void btnSettingsPressed() {
+		gameController.editSettings();
+	}
+	
 	public void setMainController(GameController gameController) {
 		this.gameController = gameController;
+		btnPlay.requestFocus();
 	}
 	
 }
