d2270ffd33e43f9db9c08b6810fb13ba688ec8f7
Sven Schirmer <sven.glueckspilz@mastersong.de>
2020-08-17 13:44:02 +0200

    bugfixes in temperatur logger

--- log_temperature.sh
+++ log_temperature.sh
@@ -2,6 +2,6 @@
 
 t_now=$(vcgencmd measure_temp | grep -Eo '[0-9]*\.[0-9]')°C
 t_date=$(date +"%Y-%m-%d %H:%M:%S")
-echo $t_now $t_date >> raspitemperatur.txt
+echo $t_now $t_date >> /home/pi/raspitemperatur.txt
 
-# as cronjob: */5 * * * * /home/pilog_temperature.sh
+# as cronjob: */5 * * * * /home/pi/log_temperature.sh
